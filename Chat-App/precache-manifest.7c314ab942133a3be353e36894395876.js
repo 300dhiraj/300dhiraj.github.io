f4aef828f2b0d73d0806d2300de884e807fd1609
dnesarkar_apttus <dnesarkar@apttus.com>
2020-04-22 18:48:30 +0530

    chat app

---
+++ Chat-App/precache-manifest.7c314ab942133a3be353e36894395876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aaa0c58754f62e1cabbdcd1db92eb71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50ba4acf0250bd66a2b5",
+    "url": "/static/css/main.88a9353f.chunk.css"
+  },
+  {
+    "revision": "151c70f95579d50f58b6",
+    "url": "/static/js/2.471f6b59.chunk.js"
+  },
+  {
+    "revision": "50ba4acf0250bd66a2b5",
+    "url": "/static/js/main.d5b38f1d.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
