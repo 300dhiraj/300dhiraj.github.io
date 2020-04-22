71a53449c47c6a6a644a98a496363478d13d2067
dnesarkar_apttus <dnesarkar@apttus.com>
2020-04-22 19:34:43 +0530

    update

--- Chat-App/service-worker.js
+++ Chat-App/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "../precache-manifest.f02a746cf96e7204f9f7dbddaaf32814.js"
+  "./precache-manifest.1124c097432d2fda03a8bb4155362b02.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
